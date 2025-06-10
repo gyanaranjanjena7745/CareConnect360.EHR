5f41b5072eca4d9b4985cb08654b63eaa65efe7b
gyanaranjanjena7745 <gyanaranjanjena7745@gmail.com>
2025-06-10 16:18:22 +0530

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CareConnect360.EHR
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
